bb812836ac582c77052efccdfc28302a6a364f7c
keerthana-sasikumar <keerthanasasikumar08@gmail.com>
2025-12-29 19:26:43 +0530

    Implement quadratic equation root calculator
    This program calculates the roots of a quadratic equation based on user input for coefficients a, b, and c. It handles both real and imaginary roots.

---
+++ p4.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    float a, b, c, d, r1, r2;
+
+    cout << "Enter a, b, c: ";
+    cin >> a >> b >> c;
+
+    d = b*b - 4*a*c;
+
+    if (d >= 0) {
+        r1 = (-b + sqrt(d)) / (2*a);
+        r2 = (-b - sqrt(d)) / (2*a);
+        cout << "Roots are: " << r1 << " and " << r2;
+    } else {
+        cout << "Roots are imaginary";
+    }
+
+    return 0;
+}
